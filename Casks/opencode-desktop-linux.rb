03aecbfcc59174cffd1efd1d7cbe54714e45724a
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-04-21 04:05:17 +0000

    opencode-desktop-linux 1.14.19

--- Casks/opencode-desktop-linux.rb
+++ Casks/opencode-desktop-linux.rb
@@ -1,9 +1,9 @@
 cask "opencode-desktop-linux" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.3.13"
-  sha256 arm64_linux:  "0772d48eccdd247a3fba1dd8368a68dcf6944dc4bc76adc2365a39adfc52b337",
-         x86_64_linux: "9edeb1bf1a04eef821d34df2cfaecec6947ddef7926ea3ce9b41784e330cea96"
+  version "1.14.19"
+  sha256 arm64_linux:  "988093dbc39100ec7fd0af4c1a63dd0f451dc28f7ae12754ac2fec31c17e0619",
+         x86_64_linux: "08cfa1e61f2a187e2ff958c93aed5577d482a15dea5ecf5e9ca0485959a5bf2c"
 
   url "https://github.com/anomalyco/opencode/releases/download/v#{version}/opencode-desktop-linux-#{arch}.rpm",
       verified: "github.com/anomalyco/opencode/"
