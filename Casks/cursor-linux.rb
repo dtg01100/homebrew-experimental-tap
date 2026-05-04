6fb2d915fe8308a7cd9c09a9cc7a5141560eb7fc
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-05-04 04:20:12 +0000

    cursor-linux 3.2.21

--- Casks/cursor-linux.rb
+++ Casks/cursor-linux.rb
@@ -2,9 +2,9 @@ cask "cursor-linux" do
   arch arm: "arm64", intel: "x64"
   file_arch = on_arch_conditional arm: "aarch64", intel: "x86_64"
 
-  version "2.6.22,c6285feaba0ad62603f7c22e72f0a170dc8415a5"
-  sha256 arm64_linux:  "90c5e6803cdaf409dd54828450561a3f1397e42ef07cd63a51658aa9d5f71251",
-         x86_64_linux: "ae63d7069207229f3bc85d0f0c986a220bed5ce5584f42571d3eba6329f08208"
+  version "3.2.21,806df57ed3b6f1ee0175140d38039a38574ec722"
+  sha256 arm64_linux:  "db35a5d59f9e0a790630c0453a700d58ab7b8f0ca49434e82a742013a9a0620f",
+         x86_64_linux: "9e41b1a68e41b29d89b5e5f1989e32d81fdaa7cc377fde6664840186e1b25302"
 
   url "https://downloads.cursor.com/production/#{version.csv.second}/linux/#{arch}/Cursor-#{version.csv.first}-#{file_arch}.AppImage",
       verified: "downloads.cursor.com/"
