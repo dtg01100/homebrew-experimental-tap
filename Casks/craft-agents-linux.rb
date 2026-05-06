7cd6475404f367b3ac87ffed981b491a4a014175
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-05-06 04:16:42 +0000

    craft-agents-linux 0.9.1

--- Casks/craft-agents-linux.rb
+++ Casks/craft-agents-linux.rb
@@ -1,6 +1,6 @@
 cask "craft-agents-linux" do
-  version "0.8.2"
-  sha256 "c391e37dc106b92818c2cfaa8d55af1c672a1870d87ac8a0def062dae3d9cfa2"
+  version "0.9.1"
+  sha256 "4280ef73fef3006e8d403c3b4db772a143d6f9401f23afbeb2c8e5e01cb5360b"
 
   url "https://github.com/lukilabs/craft-agents-oss/releases/download/v#{version}/Craft-Agents-#{version}-linux-x64.AppImage"
   name "Craft Agents"
@@ -27,7 +27,8 @@ cask "craft-agents-linux" do
     icons_dir = "#{Dir.home}/.local/share/icons/hicolor/512x512/apps"
     apps_dir = "#{Dir.home}/.local/share/applications"
     FileUtils.mkdir_p(icons_dir)
-    FileUtils.cp("#{staged_path}/squashfs-root/usr/share/icons/hicolor/512x512/apps/@craft-agentelectron.png", icons_dir)
+    FileUtils.cp("#{staged_path}/squashfs-root/usr/share/icons/hicolor/512x512/apps/@craft-agentelectron.png",
+                 icons_dir)
     FileUtils.cp("#{staged_path}/squashfs-root/@craft-agentelectron.desktop", apps_dir)
     desktop_path = "#{apps_dir}/@craft-agentelectron.desktop"
     if File.exist?(desktop_path)
