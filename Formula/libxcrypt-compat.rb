230ed02e06cbef5e43f5b4ce158e3afb37d734e5
Ahmed Adan <ahmed.adan@gmail.com>
2026-08-25 04:06:09 +0000

    libxcrypt-compat: add 4.5.2 bottle.

--- Formula/libxcrypt-compat.rb
+++ Formula/libxcrypt-compat.rb
@@ -5,6 +5,12 @@ class LibxcryptCompat < Formula
   sha256 "71513a31c01a428bccd5367a32fd95f115d6dac50fb5b60c779d5c7942aec071"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    root_url "https://github.com/ublue-os/homebrew-experimental-tap/releases/download/libxcrypt-compat-4.5.2"
+    sha256 cellar: :any, arm64_linux:  "c648502316caefed68f91f52f421d979e662da1e7fbbb1ad25801f7f89214640"
+    sha256 cellar: :any, x86_64_linux: "19a35f19c6c15b55c4a7b7d0a2a8affcf98884252a07a7cc507525580e3bdf11"
+  end
+
   keg_only "it provides the legacy libcrypt.so.1 ABI"
 
   depends_on :linux
